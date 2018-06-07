c301b1a28ce5d29ef825fb107be0974bef24551a
andrepet <pettersson.andreas.lennart@gmail.com>
2018-06-07 17:22:10 +0200

    Add dummy code using lanterna-terminal

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+import com.googlecode.lanterna.TerminalFacade;
+import com.googlecode.lanterna.input.Key;
+import com.googlecode.lanterna.terminal.Terminal;
+
+import java.nio.charset.Charset;
+
+public class Main {
+    public static void main(String[] args){
+        Terminal terminal = TerminalFacade.createTerminal(System.in,
+                System.out, Charset.forName("UTF8"));
+        terminal.enterPrivateMode();
+        while(true){
+            //Wait for a key to be pressed
+            Key key;
+            do{
+                try{
+                    Thread.sleep(5);
+                }
+                catch(InterruptedException e){
+                    e.printStackTrace();
+                }
+                key =terminal.readInput();
+            }
+            while(key == null);
+            System.out.println(key.getCharacter()+ " " + key.getKind());
+        }
+    }
+}
+
